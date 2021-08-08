283e7b6103fb05a283fd29f89a073c9d20659811
zoumson <stargue49@gmail.com>
2021-08-09 03:22:00 +0800

    remove std

--- src/imageBasics.cpp
+++ src/imageBasics.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <chrono>
-using namespace std;
+
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -26,53 +26,56 @@ int main ( int argc, char** argv )
     {
         parser.printErrors();
         return -1;
-    }
-    cout << imagePath;
-    // 读取argv[1]指定的图像
+    }    
    
-    cv::Mat image;
-
-    image = cv::imread (imagePath); // read image from path 
+    cv::Mat image = cv::imread (imagePath); // read image from path 
 
     // Image dimension 
-    cout<<"Image column"<<image.cols<<",row"<<image.rows<<",channels"<<image.channels()<<endl;
+    std::cout<<"Image column"<<image.cols<<",row"<<image.rows<<",channels"<<image.channels()<<"\n";
     cv::imshow ( "image", image );      // Show image to user
     cv::waitKey ( 0 );                  // to see image from window add a wait 
     // Check image type
     if ( image.type() != CV_8UC1 && image.type() != CV_8UC3 )
     {
 
-        cout<<"image type not supported."<<endl;
+        std::cout<<"image type not supported."<<"\n";
         return 0;
     }
 
-    // Traverse the image, please note that the following traversal methods can also be used for random pixel access
-     // Use std::chrono to time the algorithm
-    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    // Traverse the image, please note that the following traversal methods 
+    // can also be used for random pixel access
+    // Use std::chrono to compute the algorithm duration
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     for ( size_t y=0; y<image.rows; y++ )
     {
         // Use cv::Mat::ptr to get the line pointer of the image
-        unsigned char* row_ptr = image.ptr<unsigned char> ( y );  // row_ptr single line pointer to image 
-        for ( size_t x=0; x<image.cols; x++ )// traverse colum
+        // row_ptr single line pointer to image 
+        unsigned char* row_ptr = image.ptr<unsigned char> ( y ); 
+        // traverse column
+        for ( size_t x=0; x<image.cols; x++ )
         {
             // Access the pixel at x,y
-            unsigned char* data_ptr = &row_ptr[ x*image.channels() ]; // data_ptr Point to the pixel data to be accessed
-            // Each channel of the output pixel, if it is a grayscale image, there is only one channel
+            // data_ptr Point to the pixel data to be accessed
+            unsigned char* data_ptr = &row_ptr[ x*image.channels() ]; 
+            // Each channel of the output pixel, if it is a grayscale image, 
+            // there is only one channel
             for ( int c = 0; c != image.channels(); c++ )
             {
-                unsigned char data = data_ptr[c]; // data is the value of the c channel of I(x,y)
+                // data is the value of the c channel of I(x,y)
+                unsigned char data = data_ptr[c]; 
             }
         }
     }
-    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>( t2-t1 );
-    cout<<"Time to traverse the image："<<time_used.count()<<" second."<<endl;
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> time_used = std::chrono::duration_cast<std::chrono::duration<double>>( t2-t1 );
+    std::cout<<"Time to traverse the image："<<time_used.count()<<" second."<<"\n";
 
     // About the copy of cv::Mat
     // Direct assignment does not copy data
     cv::Mat image_another = image;
     // Modifying image_another will cause the image to change
-    image_another ( cv::Rect ( 0,0,100,100 ) ).setTo ( 0 ); // Zero the 100*100 block in the upper left corner
+    // Zero the 100*100 block in the upper left corner
+    image_another ( cv::Rect ( 0,0,100,100 ) ).setTo ( 0 );
     cv::imshow ( "image", image );
     cv::waitKey ( 0 );
     
@@ -85,7 +88,8 @@ int main ( int argc, char** argv )
 
     // There are many basic operations for images, such as cutting, rotating, zooming, etc., 
     // which are not introduced one by one due to space limitations.
-    // Please refer to the official OpenCV documentation to find out how to call each function.
+    // Please refer to the official OpenCV documentation to 
+    // find out how to call each function.
     cv::destroyAllWindows();
 
     return 0;
